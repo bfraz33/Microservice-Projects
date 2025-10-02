1b63f0fc05030fe685cad82fefb36a4190ece4f2
Brandon Frazier <bfrazier756@gmail.com>
2025-10-01 21:29:22 -0500

    corrected small errors.

--- quotes.py
+++ quotes.py
@@ -3,14 +3,14 @@ import random
 
 app = FastAPI()
 
-QUOTES = [
-        "I have a dream. - Martin Luther King Jr."
-        "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela"
-        "The way to get started is to quit talking and begin doing.  - Walt Disney"
-        "To be, or not to be, that is the question.  - William Shakespeare"
-        "A journey of a thousand miles begins with a single step.  - Lao Tzu"
+quotes = [
+        "I have a dream. - Martin Luther King Jr.",
+        "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
+        "The way to get started is to quit talking and begin doing.  - Walt Disney",
+        "To be, or not to be, that is the question.  - William Shakespeare",
+        "A journey of a thousand miles begins with a single step.  - Lao Tzu",
 ]
 
 @app.get("/quotes")
-def get_quotes(quotes):
-    return {"Quotes:" : random.choice(QUOTES)}
+def get_quotes():
+    return {"Quotes:" : random.choice(quotes)}
